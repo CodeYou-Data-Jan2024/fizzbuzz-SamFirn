da0e6e12452373fcb19eb5fc0e72e80e4f00f572
Sam Firn <samfirn@gmail.com>
2024-04-01 17:26:47 -0400

    hope this works

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,16 @@
-# add your code here
 
+x = range(1, 101)
+
+def Fizzle(x):
+    
+    for i in x:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+
+Fizzle(x)
